6ac9ac550a46dce296af41ed9428408308668463
baby-adigopula <96115223+baby-adigopula@users.noreply.github.com>
2022-02-16 10:01:43 +0530

    Initial Version

---
+++ LegacyIntegrator/IISBlobAdapter/IISBlobDetails.cs
@@ -0,0 +1,27 @@
+﻿/****************************************************************
+Copyright 2021 Infosys Ltd. 
+Use of this source code is governed by Apache License Version 2.0 that can be found in the LICENSE file or at 
+http://www.apache.org/licenses/
+ ***************************************************************/
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace Infosys.Lif.LegacyIntegratorService
+//{
+//    public class IISDoc
+//    {
+//        public string DllPath { get; set; }
+//        public string TypeName { get; set; }
+//        public List<IISDocDetails> IISBlobDetails { get; set; }
+//    }
+
+//    public class IISDocDetails
+//    {
+//        public string TransportName { get; set; }
+//        //public string RootDNS { get; set; }
+//        public int DataBlockSize {get;set;}
+//        public string DocumentsVirtualDirectoryFromRoot { get; set; }
+//    }
+//}
