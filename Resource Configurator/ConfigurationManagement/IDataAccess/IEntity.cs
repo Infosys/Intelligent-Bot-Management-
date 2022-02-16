1cc710e1d2e3ce32aaea6fb54764f72926867ce4
baby-adigopula <96115223+baby-adigopula@users.noreply.github.com>
2022-02-16 08:20:18 +0530

    Initial version

---
+++ Resource Configurator/ConfigurationManagement/IDataAccess/IEntity.cs
@@ -0,0 +1,26 @@
+﻿/****************************************************************
+Copyright 2021 Infosys Ltd. 
+Use of this source code is governed by Apache License Version 2.0 that can be found in the LICENSE file or at 
+http://www.apache.org/licenses/
+ ***************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infosys.Solutions.ConfigurationManager.Resource.IDataAccess
+{
+    public interface IEntity<T>
+    {
+        T GetOne(T Entity);
+        IQueryable<T> GetAny();
+        IList<T> GetAll();
+        T Insert(T entity);
+        T Update(T entity);
+        bool Delete(T entity);
+        IList<T> InsertBatch(IList<T> entities);
+        IList<T> UpdateBatch(IList<T> entities);
+        IList<T> GetAll(T Entity);
+    }
+}
